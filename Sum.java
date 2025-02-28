beb8c72f5329b059d6d4cb2ee407edfd321cd297
Shailendrarai855 <77872446+Shailendrarai855@users.noreply.github.com>
2025-02-28 12:26:35 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -5,7 +5,6 @@ public class Sum {
         int a = 10;
         int b = 20;
         System.out.println(a + b);
-        System.out.println("new update");
         System.out.println("github webHook Inteegeration cheking final map lga k ab json m convert kia hai");
     }
 }
